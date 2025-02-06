7cb05fb9f5f74ebbbb4c0fd97778648aee74055f
Rayane BELABBAS <rayanebelabbas15@gmail.com>
2025-02-06 15:31:31 +0100

    add env

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '27534f15f9610c9b081d41c5e2f5bee90362c826',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '27534f15f9610c9b081d41c5e2f5bee90362c826',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
